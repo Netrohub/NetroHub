547288f37c3c40091b91d62e0b6d70af3ddae089
netrohub <lilnetro305@gmail.com>
2025-10-18 15:08:52 +0300

    fix: Filament navigationGroup type declaration for PHP 8.2+ compatibility

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
 
-    protected static ?string $navigationGroup = 'User Management';
+    protected static string $navigationGroup = 'User Management';
 
     protected static ?int $navigationSort = 1;
 
