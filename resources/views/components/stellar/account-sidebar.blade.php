0b13f99b7a35154cec795824f2781db1fd076faf
netrohub <lilnetro305@gmail.com>
2025-10-18 18:37:31 +0300

    fix: replace x-collapse with x-transition to remove Alpine.js plugin dependency
    - Replaced x-collapse directive with x-transition in account-sidebar.blade.php
    - Replaced x-collapse directive with x-transition in pricing/index.blade.php
    - Added proper enter/leave transitions for smooth animations
    - This eliminates the need for the Alpine.js Collapse plugin

--- resources/views/components/stellar/account-sidebar.blade.php
+++ resources/views/components/stellar/account-sidebar.blade.php
@@ -14,10 +14,12 @@
             </svg>
         </button>
         <div x-show="open" 
-             x-collapse
              x-transition:enter="transition ease-out duration-200"
              x-transition:enter-start="opacity-0 -translate-y-2"
              x-transition:enter-end="opacity-100 translate-y-0"
+             x-transition:leave="transition ease-in duration-150"
+             x-transition:leave-start="opacity-100 translate-y-0"
+             x-transition:leave-end="opacity-0 -translate-y-2"
              class="mt-2 bg-slate-800/50 rounded-xl border border-slate-700/50 overflow-hidden">
             @include('components.stellar.account-nav')
         </div>
--- resources/views/pricing/index.blade.php
+++ resources/views/pricing/index.blade.php
@@ -167,7 +167,12 @@
                         </button>
                         <div 
                             x-show="activeIndex === {{ $index }}"
-                            x-collapse
+                            x-transition:enter="transition ease-out duration-200"
+                            x-transition:enter-start="opacity-0 -translate-y-2"
+                            x-transition:enter-end="opacity-100 translate-y-0"
+                            x-transition:leave="transition ease-in duration-150"
+                            x-transition:leave-start="opacity-100 translate-y-0"
+                            x-transition:leave-end="opacity-0 -translate-y-2"
                             class="px-6 pb-4">
                             <p class="text-slate-400">{{ $faq['a'] }}</p>
                         </div>
